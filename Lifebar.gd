da7ce31b1fd591f24ca891e12e883c6f07b037e4
Matheus Victor <matheusvictor870@gmail.com>
2019-10-01 22:02:13 -0300

    Projeto organizado e BulletSystem atualizado

--- Lifebar.gd
+++
@@ -1,8 +0,0 @@
-extends PanelContainer
-
-onready var bar = $TextureProgress
-
-func _ready():
-	player_stats.connect("health_updated", self, "render_health")
-func render_health():
-	bar.decrease_life(player_stats.health)
