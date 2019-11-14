297f4ee02947cc7630cbfa8ea8dadbcde8525abe
Matheus Victor <matheusvictor870@gmail.com>
2019-11-14 01:21:40 -0300

    Menos erros no output dos testes

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,1 @@
-Godot_v3.1.1-stable_linux_headless.64 -s addons/gut/gut_cmdln.gd --no-window -d --path $PWD -gdir=res://unit_testing/Unit/ -gprefix=test -gexit
+Godot_v3.1.1-stable_linux_headless.64 -s addons/gut/gut_cmdln.gd --no-window -d --path $PWD -gdir=res://unit_testing/Unit/ -gprefix=test -gexit | grep -v "ERROR: "
