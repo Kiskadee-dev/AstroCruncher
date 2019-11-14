4f8a388b1bce8d7bfa066d0a923ecdbc2b7b6b48
Matheus Victor <matheusvictor870@gmail.com>
2019-11-14 09:11:18 -0300

    Added export option to drone

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,1 @@
-Godot_v3.1.1-stable_linux_headless.64 -s addons/gut/gut_cmdln.gd --no-window -d --path $PWD -gdir=res://unit_testing/Unit/ -gprefix=test -gexit | grep -v "ERROR: "
+Godot_v3.1.1-stable_linux_headless.64 -s addons/gut/gut_cmdln.gd -d --path . -gdir=res://unit_testing/Unit/ -gprefix=test -gexit
