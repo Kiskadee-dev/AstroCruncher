0c44a64c10a0570417eeda25441f0544d2c29efc
Matheus Victor <matheusvictor870@gmail.com>
2019-11-15 08:57:19 -0300

    Sem debug para o teste

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,1 @@
-Godot_v3.1.1-stable_linux_headless.64 -s addons/gut/gut_cmdln.gd -d --path . -gdir=res://unit_testing/Unit/ -gprefix=test -gexit
+Godot_v3.1.1-stable_linux_headless.64 -s addons/gut/gut_cmdln.gd --path . -gdir=res://unit_testing/Unit/ -gprefix=test -gexit
