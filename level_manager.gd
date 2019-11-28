641314bc87d3fba00e22616cca80c3339f12b2e1
Matheus Victor <matheusvictor870@gmail.com>
2019-11-28 17:35:43 -0300

    Adicionado opção para fade in/out

--- level_manager.gd
+++ level_manager.gd
@@ -1,9 +1,16 @@
 extends Node
 
 var levels = {"0":"res://Scenes/test.tscn"}
+var loading_something:bool = false
 
 func start_level(n:int):
-	get_tree().change_scene(levels[str(n)])
+	if not loading_something:
+		loading_something = true
+		FadeSystem.fade_out()
+		yield(FadeSystem, "done")
+		get_tree().change_scene(levels[str(n)])
+		FadeSystem.fade_in(1)
+		loading_something = false
 
 func exit_game():
 	get_tree().quit()
